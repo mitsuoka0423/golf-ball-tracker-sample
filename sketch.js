9f58b56904b66d8ecb42dec0432124a66640bd31
takahiro mitsuoka <tmitsuoka0423@gmail.com>
2020-11-29 13:04:54 +0900

    add

---
+++ sketch.js
@@ -0,0 +1,83 @@
+// Copyright (c) 2019 ml5
+//
+// This software is released under the MIT License.
+// https://opensource.org/licenses/MIT
+
+/* ===
+ml5 Example
+Real time Object Detection using objectDetector
+=== */
+
+let objectDetector;
+let status;
+let objects = [];
+let canvas, ctx;
+const width = 640;
+const height = 420;
+
+async function make() {
+  img = new Image();
+  img.src = 'images/ball2.jpg';
+  img.width = width;
+  img.height = height;
+
+  objectDetector = await ml5.objectDetector('yolo', startDetecting)
+  
+  canvas = createCanvas(width, height);
+  ctx = canvas.getContext('2d');
+}
+
+// when the dom is loaded, call make();
+window.addEventListener('DOMContentLoaded', function() {
+  make();
+});
+
+function startDetecting(){
+  console.log('model ready')
+  detect();
+}
+
+function detect() {
+  objectDetector.detect(img, function(err, results) {
+    if(err){
+      console.log(err);
+      return
+    }
+
+    console.log(results);
+    objects = results;
+
+    if(objects){
+      draw();
+    }
+  });
+}
+
+function draw(){
+  // Clear part of the canvas
+  ctx.fillStyle = "#000000"
+  ctx.fillRect(0,0, width, height);
+
+  ctx.drawImage(img, 0, 0);
+  for (let i = 0; i < objects.length; i += 1) {
+      
+    ctx.font = "16px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText(objects[i].label, objects[i].x + 70, objects[i].y); 
+
+    ctx.beginPath();
+    ctx.rect(objects[i].x + 70, objects[i].y, objects[i].width, objects[i].height);
+    ctx.strokeStyle = "white";
+    ctx.stroke();
+    ctx.closePath();
+  }
+}
+
+
+function createCanvas(w, h){
+  const canvas = document.createElement("canvas"); 
+  canvas.width  = w;
+  canvas.height = h;
+  document.body.appendChild(canvas);
+  return canvas;
+}
